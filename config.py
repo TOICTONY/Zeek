1bccf0f43d23075375afd9ba07b02811525c65f8
TOICTONY <114583077+TOICTONY@users.noreply.github.com>
2024-02-14 13:22:46 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6337794841:AAHTrEE8FSeLfaPOQoE3p9GZ8Va1Ug6yH2E")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6891596024:AAGBsYPcQI75ULUXO0LwarvJLaeg72_kcSY")
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID",'18312660'))
-    API_HASH = os.environ.get("API_HASH", "b0697229f0da116e4ce97018ad6df3d0")
+    APP_ID = int(os.environ.get("APP_ID",'13209296'))
+    API_HASH = os.environ.get("API_HASH", "7f94729e7d7f60aff21bb986bc068bd9")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1110545535").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "683443719").split())
 
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
@@ -20,7 +20,7 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
     # Telegram maximum file upload size
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 4194304000
 
     # chunk size that should be used with requests
     CHUNK_SIZE = 128
@@ -29,5 +29,5 @@ class Config(object):
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
     
     # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://realhulk6:karishama786@cluster0.ryimidk.mongodb.net/?retryWrites=true&w=majority")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MoviiWrld:MoviiWrld01@cluster0.lloilyc.mongodb.net/?retryWrites=true&w=majority")
     
